c6236314182dbcd9d9b6afa9e13459b21c7ae8f8
krutto89 <sitb01-028692022@student.mmust.ac.ke>
2024-11-07 21:30:13 +0300

    added about page

--- patientapp/urls.py
+++ patientapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home, name='home'),
+    path('about/', views.about, name='about'),
 
 ]
--- patientapp/views.py
+++ patientapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'patientapp/index.html')
+
+def about(request):
+    return render(request,'patientapp/about.html')
